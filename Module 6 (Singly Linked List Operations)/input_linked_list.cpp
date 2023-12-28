4ffa23d15d59e87363948de45a854ac7afc45822
Shuvo <shuvo92373@gmail.com>
2023-12-29 02:50:35 +0600

    complete module 6

---
+++ Module 6 (Singly Linked List Operations)/input_linked_list.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val=val;
+            this->next=NULL;
+        }
+};
+
+// insert at tail 
+void insert_at_tail(Node * &head, int v)
+{
+    Node *newNode = new Node(v);
+    if(head==NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    // last e node ekon tmp
+    tmp->next = newNode;
+}
+
+// print linked list
+void print_linked_list(Node *head)
+{
+    cout << endl;
+    cout << "Your Linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl << endl;
+    
+}
+int main()
+{
+    int val;
+    Node *head = NULL;
+    while (true)
+    {
+        cin >> val;
+        if(val==-1)
+        {
+            break;
+        }
+        insert_at_tail(head, val);
+    }
+    print_linked_list(head);
+
+    
+    return 0;
+}
